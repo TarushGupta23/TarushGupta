d5826c32011839d1f22294b0de5f43225201894e
TarushGupta23 <tarush23.gupta@gmail.com>
2023-12-30 17:38:13 +0530

    restart website

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import ejs from "ejs"
+import bodyParser from "body-parser";
+
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.listen(port)
