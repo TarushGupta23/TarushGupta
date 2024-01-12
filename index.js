36b9704314929f81ed520a2bb5a32bf8668f8fd9
TarushGupta23 <tarush23.gupta@gmail.com>
2024-01-12 16:18:58 +0530

    update hero page

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ import express from "express"
 import bodyParser from "body-parser";
 
 const app = express()
-const port = 3000
+const port = 3080
 
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
+})
+
+app.get("/test", (req, res) => {
+    res.render("testing.ejs")
 })
 
 
